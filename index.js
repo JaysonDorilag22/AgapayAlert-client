1520f51e5b04e25863f07dc3216f61f330b81a7d
JD <jaysondorilag1@gmail.com>
2024-12-19 21:08:24 +0800

    Integrate Redux for state management and update app entry point

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 import { registerRootComponent } from 'expo';
-
+import { Provider } from 'react-redux';
 import App from './App';
+import store from './redux/store';
 
-// registerRootComponent calls AppRegistry.registerComponent('main', () => App);
-// It also ensures that whether you load the app in Expo Go or in a native build,
-// the environment is set up appropriately
-registerRootComponent(App);
+const Root = () => (
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
+
+registerRootComponent(Root);
