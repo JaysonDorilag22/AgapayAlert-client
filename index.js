1270eb011867e431734293b1a53d0485708eb341
JD <jaysondorilag1@gmail.com>
2024-12-20 22:12:45 +0800

    Update import path for Redux store in index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import { registerRootComponent } from 'expo';
 import { Provider } from 'react-redux';
 import App from './App';
-import store from './redux/store';
+import store from './src/redux/store';
 
 const Root = () => (
   <Provider store={store}>
