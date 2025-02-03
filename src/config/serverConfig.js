889beb5b62cadea34138288e3b8223a543ca515f
JD <jaysondorilag1@gmail.com>
2025-02-03 22:58:11 +0800

    Update app name and change server configuration URLs to production

--- src/config/serverConfig.js
+++ src/config/serverConfig.js
@@ -1,7 +1,7 @@
 const serverConfig = {
-  baseURL: 'http://192.168.254.157:3000/api/v1',
-  messengerURL: 'http://192.168.254.157:3000/api',
-  socketURL: 'http://192.168.254.157:3000'
+  baseURL: 'https://agapayalert-server.onrender.com/api/v1',
+  messengerURL: 'https://agapayalert-server.onrender.com/api',
+  socketURL: 'https://agapayalert-server.onrender.com'
 };
 
 export default serverConfig;
